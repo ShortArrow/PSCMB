1351bf3e6098ac360f7b73568f280917f8c2e4e5
ShortArrow <bamboogeneral@live.jp>
2021-10-26 09:32:49 +0900

    change directory installers

--- bin/install.cmd
+++ bin/install.cmd
@@ -1,7 +1,6 @@
 @echo off
 echo "setup PSCMB"
 pushd %~dp0
-cd ..
 pwsh -NoProfile -ExecutionPolicy Unrestricted "./Install-To-ContextMenu.ps1"
 pause > nul
 exit
--- bin/uninstall.cmd
+++ bin/uninstall.cmd
@@ -1,7 +1,6 @@
 @echo off
 echo "uninstall PSCMB"
 pushd %~dp0
-cd ..
 pwsh -NoProfile -ExecutionPolicy Unrestricted "./Uninstall-ContextMenu.ps1"
 pause > nul
 exit
